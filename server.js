1ad4a247bc5551bb4320494baecb996aab9d139f
Novan <rachadian.novansyah15@gmail.com>
2020-04-10 11:27:00 +0700

    Bismillah Node JS (package + server)

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:7001"
+};
+
+app.use(cors(corsOptions));
+
+// for request of content-type - application/json
+app.use(bodyParser.json());
+
+// for request of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended:true }));
+
+// for route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to paparazi app with nodejs"});
+});
+
+// set port, and listen for requests
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
